677dc11d5162648cb99325548a19743e840bc7bd
Nyappy Chan <nothingklara@gmail.com>
2023-02-06 11:40:13 +0100

    fix: testing another fix

--- src/scripts.sh
+++ src/scripts.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
 # get to the right repo
 cd DD2480-CI
 
 # compile all with py_compile
 python3 -m py_compile src/*.py
 
-[ $? -eq 1 ] && exit 1 || exit 0
+if [ $? -eq 1 ]
+then 
+    exit 1
+fi
 
 # all is well
-#exit 0
+exit 0

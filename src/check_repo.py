2781ba58d6efa1b32e1f7f9cc68bda2b2810bddb
asbai <asbai@kth.se>
2023-02-06 15:35:52 +0100

    fix altered check_repo method to work correctly

--- src/check_repo.py
+++ src/check_repo.py
@@ -5,8 +5,15 @@ def check(url, repo, branch):
     s.run(['git', 'clone', url])                                 
     s.run(['git', '-C', repo, 'checkout', branch])           
     
-    if(s.run(['test', '-f', 'src/scripts.sh']) == 1):
-        return {'returncode': 3, 'stderr': 'Error: Missing compilation/test script file.'}
+    try:
+        s.run(['test', '-f', 'src/scripts.sh'])
+    except IOError:
+        class Status(object):
+            pass
+        status = Status()
+        status.returncode = 3
+        status.stderr = 'Error: Missing compilation/test script file.'
+        return status
     
     status = s.run(['sh', repo + '/src/scripts.sh'])        
     s.run(['rm', '-rf', repo])

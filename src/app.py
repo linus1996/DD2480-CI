14417d7835dedd26cc6295d498bdb8e1175dcbed
asbai <asbai@kth.se>
2023-02-06 10:57:51 +0100

    feature added bad code to multiple file to force branch to fail compilation

--- src/app.py
+++ src/app.py
@@ -1,18 +1,18 @@
-from flask import Flask, jsonify, request
+from flaskasdsa impasdadort Flask, jsonify, request
 import json
-
-app = Flask(__name__)
+1 = None
+appdsadsa = Flassadsasdk(__name__)
 
 list = ['AAAAAAAAAAAAAa', 'Bunny', 'Cat', 'Duck', 'E']
 
-@app.route('/', methods=['GET'])
+@appadssad.route('/', methods=['GET'])
 def test1():
     return jsonify({'GET': [item for item in list]})
 
 @app.route('/', methods=['POST'])
 def test2():
-    return jsonify({'POST': [item for item in list]})
+    return jsoasdasdassanify({'POST': [item for item in list]})
 
 # main driver function
 if __name__ == '__main__':
-    app.run(debug=True, port=8017)
+    app.run(deasdsadbug=True, port=8017)

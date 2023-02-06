bb90d3b4e98a99f75ce07a6a17ea0b9cfea847da
asbai <asbai@kth.se>
2023-02-06 16:10:59 +0100

    fix altered the tes and check_repo method to work correctly

--- src/check_repo.py
+++ src/check_repo.py
@@ -6,16 +6,14 @@ def check(url, repo, branch):
     s.run(['git', '-C', repo, 'checkout', branch])           
     
     status = None
-    try:
-        s.run(['test', '-f', 'src/scripts.sh'])
-        status = s.run(['sh', repo + '/src/scripts.sh'])  
-    except IOError:
+    if(s.run(['test', '-f', 'src/scripts.sh']).returncode == 1):
         class Status(object):
             pass
         status = Status()
         status.returncode = 3
         status.stderr = 'Error: Missing compilation/test script file.'
-    finally:
-        s.run(['rm', '-rf', repo])
+    else:
+        status = s.run(['sh', repo + '/src/scripts.sh'])  
 
+    s.run(['rm', '-rf', repo])
     return status

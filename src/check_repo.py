67f6facd4548129a19f14318ccff2c05f10daca7
asbai <asbai@kth.se>
2023-02-06 09:51:32 +0100

    fix changed input variables for check method

--- src/check_repo.py
+++ src/check_repo.py
@@ -1,19 +1,14 @@
 import json
 import subprocess as s
 
-def check(payload):                                                        
-                                                                                                                             
-    branch = payload['ref'].split('/')[-1]
-    url = payload['repository']['clone_url']
-    repo = payload['repository']['name']
-                                                                                                      
+def check(url, repo, branch):                                                                                                                                                                                                                                                                           
     s.run(['git', 'clone', url])                                 
     s.run(['git', '-C', repo, 'checkout', branch])           
     
     if(s.run(['test', '-f', 'scripts.sh']) == 1):
         return 'Error: Missing compilation/test script file.'
     
-    status = s.run(['sh', repo + '/scripts.sh']).returncode         
+    status = s.run(['sh', repo + '/scripts.sh'])        
     s.run(['rm', '-rf', repo])
 
     return status

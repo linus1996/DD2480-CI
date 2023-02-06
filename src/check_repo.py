b4d91e41b7f6289daa6767fb50c6c672567474b3
asbai <asbai@kth.se>
2023-02-06 15:42:50 +0100

    fix altered check_repo method some more to handle exception cases

--- src/check_repo.py
+++ src/check_repo.py
@@ -5,17 +5,17 @@ def check(url, repo, branch):
     s.run(['git', 'clone', url])                                 
     s.run(['git', '-C', repo, 'checkout', branch])           
     
+    status = None
     try:
         s.run(['test', '-f', 'src/scripts.sh'])
+        status = s.run(['sh', repo + '/src/scripts.sh'])  
     except IOError:
         class Status(object):
             pass
         status = Status()
         status.returncode = 3
         status.stderr = 'Error: Missing compilation/test script file.'
-        return status
-    
-    status = s.run(['sh', repo + '/src/scripts.sh'])        
-    s.run(['rm', '-rf', repo])
+    finally:
+        s.run(['rm', '-rf', repo])
 
     return status

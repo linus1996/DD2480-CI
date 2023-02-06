2b9baa014b29d7322ac31467453ee229d7a8ec5d
asbai <asbai@kth.se>
2023-02-06 15:00:52 +0100

    feature created tests for invald script and failed tests

--- src/tests/test_check_repo.py
+++ src/tests/test_check_repo.py
@@ -1,8 +1,14 @@
-
 import unittest
 from src.check_repo import *
 
 class TestCheck_repo(unittest.TestCase):
+
+    def test_check_invalid(self):
+        url = 'https://github.com/linus1996/DD2480-CI.git'
+        repo = 'DD2480-CI'
+        branch = 'test-branch-missing-script'
+        result = check(url, repo, branch)
+        self.assertEqual(result.returncode, 0)
 
     def test_check_pos(self):
         url = 'https://github.com/linus1996/DD2480-CI.git'
@@ -17,3 +23,10 @@ class TestCheck_repo(unittest.TestCase):
         branch = 'test-branch-compilation-error'
         result = check(url, repo, branch)
         self.assertEqual(result.returncode, 1)
+
+    def test_check_neg2(self):
+        url = 'https://github.com/linus1996/DD2480-CI.git'
+        repo = 'DD2480-CI'
+        branch = 'test-branch-failed-test'
+        result = check(url, repo, branch)
+        self.assertEqual(result.returncode, 2)

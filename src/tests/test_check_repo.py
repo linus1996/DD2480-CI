2645542d2104404a08f55b0b5d7de47de1632b1f
asbai <asbai@kth.se>
2023-02-06 15:03:30 +0100

    feature added test for missing script file

---
+++ src/tests/test_check_repo.py
@@ -0,0 +1,11 @@
+import unittest
+from src.check_repo import *
+
+class TestCheck_repo(unittest.TestCase):
+
+    def test_check_invalid(self):
+        url = 'https://github.com/linus1996/DD2480-CI.git'
+        repo = 'DD2480-CI'
+        branch = 'test-branch-compilation-success'
+        result = check(url, repo, branch)
+        self.assertEqual(result.returncode, 3)

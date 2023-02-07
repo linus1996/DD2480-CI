5bd68a07ee17ba6f23e1256b27b1428d4dcbb5fd
axellervik <axel.lervikgmail.com>
2023-02-07 10:29:40 +0100

    Feature: insert entry into database

--- src/app.py
+++ src/app.py
@@ -2,8 +2,9 @@ from flask import Flask, jsonify, request, render_template
 from check_repo import check
 from communication.notifications import update_status
 from src import config
-import datetime
+from server.history import History
 
+# Application:
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -15,19 +16,19 @@ def handle_post():
     data = request.form
     # set update status to pending
     update_status(data, 'pending', config.api_token)
-    # extract the relevant data
-    clone_url = data['clone_url']
-    repo_url = data['url']
-    branch_name = data['ref'].split('/')[-1]
     # run compile script and test script
-    result = check(clone_url, repo_url, branch_name)
+    result = check(data['clone_url'], data['url'], data['ref'].split('/')[-1])
     # update status based on result
     status = 'success' if result.returncode == 0 else 'failure'
     update_status(data, status, config.api_token)
     # insert into database
-    
+    build = history.serialize(data['commit_id'], data['commits'][0]['timestamp'], status, data['commits'][0]['url'], result.stderr if result.stderr is not None else '') # TODO: check whether or not the commit extraction is correct
+    history.insert_build(build)
     return render_template('index.html')
 
 # main driver function
 if __name__ == '__main__':
+    global history
+    config.init('ci.ini')
+    history = History(config.mongo_database_name, config.mongo_ip, config.mongo_port, config.mongo_user, config.mongo_pass)
     app.run(debug=True, port=8017)

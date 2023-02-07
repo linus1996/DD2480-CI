5baece5a7a20402e3e6c64005c955332b4b2dbd3
axellervik <axel.lervikgmail.com>
2023-02-07 11:30:00 +0100

    Fix: latest version for student-shell-2

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
+import pkg_resources
+pkg_resources.require("flask==2.0.3")
 from flask import Flask, jsonify, request, render_template
 from check_repo import check
 from communication.notifications import update_status
-from src import config
+import config
 from server.history import History
 
 # Application:
@@ -9,6 +11,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def handle_get():
+    return jsonify('GET REQUEST RECEIVED')
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
@@ -24,7 +27,8 @@ def handle_post():
     # insert into database
     build = history.serialize(data['commit_id'], data['commits'][0]['timestamp'], status, data['commits'][0]['url'], result.stderr if result.stderr is not None else '') # TODO: check whether or not the commit extraction is correct
     history.insert_build(build)
-    return render_template('index.html')
+    return 'POST REQUEST PROCESSED SUCCESSFULLY'
+    # return render_template('index.html')
 
 # main driver function
 if __name__ == '__main__':

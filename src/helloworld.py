49efb9d78177a110a02f4f1260a93455f618cc6d
Axel Lervik <70861555+axellervik@users.noreply.github.com>
2023-02-09 19:33:53 +0100

    Fix: goodbye world

--- src/helloworld.py
+++ src/helloworld.py
@@ -6,18 +6,6 @@ print('hello world')
 print('hello world')
 print('hello world')
 print('hello world')
-print hello worlddd
-print heloo wrldddd
-print goodbye world
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print('hello world')
-print hello worlddd
-print heloo wrldddd
-print goodbye world
-print farewell worl
+print ('hello worlddd')
+print ('heloo wrldddd')
+print ('goodbye world')
